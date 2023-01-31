0951a010d34599b53c43ad27c7ca662bc135ee11
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-01-31 22:31:50 +0530

    For Videos

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,8 +1,37 @@
 import cv2
-
 from random import randrange
 
 trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+## for Videos
+webcam = cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read, frame = webcam.read()
+
+    grayscaled_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(randrange(128,256),randrange(128,256),randrange(128,256)),2)
+    cv2.imshow("MV Face Detector",frame)
+    key = cv2.waitKey(1)
+    # Q for Quit
+    if key==81 or key==113:
+        break
+
+webcam.release()
+
+
+
+
+
+
+
+### for Images
+"""
 
 img = cv2.imread("demo.jpg")
 
@@ -15,5 +44,5 @@ for (x,y,w,h) in face_coordinates:
 
 cv2.imshow("MV Face Detector",img)
 cv2.waitKey()
-
+"""
 print("Complete")
